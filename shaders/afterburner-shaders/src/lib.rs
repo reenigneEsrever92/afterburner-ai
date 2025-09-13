8567491a23ac69ef6e7d11a082bd66388e84e02c
Felix Marezki <fmarezki@gmail.com>
2025-09-13 16:42:47 +0200

    wip

--- shaders/afterburner-shaders/src/lib.rs
+++ shaders/afterburner-shaders/src/lib.rs
@@ -1,27 +1,31 @@
 #![cfg_attr(target_arch = "spirv", no_std)]
 #![feature(generic_const_exprs)]
 
-use afterburner_rustgpu_shared::conv2d::*;
+use afterburner_rustgpu_shared::conv2d::RustGpuConv2DParams;
 use spirv_std::{glam::UVec3, spirv};
 
 #[spirv(compute(threads(64)))]
 pub fn test(
     #[spirv(global_invocation_id)] id: UVec3,
-    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] input: &[u8],
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] _input: &[u8],
     #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] output: &mut [f32],
 ) {
     let idx = id.x as usize;
-    output[idx] = 1.0;
+    if idx < output.len() {
+        output[idx] = 1.0;
+    }
 }
 
 #[spirv(compute(threads(64)))]
 pub fn convert_u8_f32(
-    #[spirv(global_invocation_id)] id: (u32, u32, u32),
+    #[spirv(global_invocation_id)] id: UVec3,
     #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] input: &[u8],
     #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] output: &mut [f32],
 ) {
-    let idx = id.0 as usize;
-    output[idx] = input[idx] as f32;
+    let idx = id.x as usize;
+    if idx < input.len() && idx < output.len() {
+        output[idx] = input[idx] as f32;
+    }
 }
 
 #[spirv(compute(threads(64)))]
@@ -32,5 +36,8 @@ pub fn conv2d(
     #[spirv(storage_buffer, descriptor_set = 0, binding = 1)] weights: &[f32],
     #[spirv(storage_buffer, descriptor_set = 0, binding = 2)] output: &mut [f32],
 ) {
-    afterburner_rustgpu_shared::conv2d::conv2d(id.x as usize, &params, input, weights, output);
+    let idx = id.x as usize;
+    if idx < output.len() {
+        afterburner_rustgpu_shared::conv2d::conv2d(idx, &params, input, weights, output);
+    }
 }
